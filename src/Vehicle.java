a5a1d7c0f2cb3e60aa0b62e3e3059b1124181d12
Iliya Raev <insomniq4@abv.bg>
2025-04-24 20:52:13 +0300

    Create abstract class Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,13 @@
-public class Vehicle {
+abstract class Vehicle {
+    String species;
+    String registrationNumber;
+    String brand;
+    String model;
+
+    Vehicle(String species, String registrationNumber, String brand, String model) {
+        this.species = species;
+        this.registrationNumber = registrationNumber;
+        this.brand = brand;
+        this.model = model;
+    }
 }
