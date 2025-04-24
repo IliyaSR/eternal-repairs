24606f65bafd086886c0b1fa2d0d15eb5e6fb962
Iliya Raev <insomniq4@abv.bg>
2025-04-24 20:48:28 +0300

    Create abstarct class Vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,13 @@
+abstract class Vehicle {
+    String species;
+    String registrationNumber;
+    String brand;
+    String model;
+
+    Vehicle(String species, String registrationNumber, String brand, String model) {
+        this.species = species;
+        this.registrationNumber = registrationNumber;
+        this.brand = brand;
+        this.model = model;
+    }
+}
