6c2ba00b9bed4dbb815be5b533ae1bd0ad86469e
Iliya Raev <insomniq4@abv.bg>
2025-04-25 16:34:56 +0300

    Update some fields make it enum and final and create validations!

--- src/Car.java
+++ src/Car.java
@@ -6,9 +6,17 @@ class Car extends Vehicle {
         MANUAL
     }
 
+    enum TypeFuel {
+        DIESEL,
+        PETROL
+    }
+
+    final LocalDate nowDate = LocalDate.now();
+    final LocalDate negativeYear = LocalDate.parse("0000-00-00");
+
     float fuelConsumption;
     int tankCapacity;
-    int typeFuel;
+    TypeFuel typeFuel;
     int horsePower;
     GearBoxType gearBoxType;
     LocalDate dateOfCreation;
@@ -19,7 +27,7 @@ class Car extends Vehicle {
     int kilometersEngineOil;
 
     Car(String species, String registrationNumber, String brand, String model, float fuelConsumption,
-        int tankCapacity, int typeFuel, int horsePower, GearBoxType gearBoxType, LocalDate dateOfCreation,
+        int tankCapacity, TypeFuel typeFuel, int horsePower, GearBoxType gearBoxType, LocalDate dateOfCreation,
         LocalDate dateOfRegistration, LocalDate insuranceDate, LocalDate dateOfReview,
         LocalDate dateOfChangeTires, int kilometersEngineOil) {
         super(species, registrationNumber, brand, model);
@@ -59,6 +67,109 @@ class Car extends Vehicle {
     public LocalDate getDateOfReview() {
         return dateOfReview;
     }
+
+    public boolean validateSpecies(String species) {
+        if (species.length() > 100) {
+            System.out.println("Species can't be longer than 100 chars!");
+            return false;
+        }
+        if (species.isBlank()) {
+            System.out.println("Species can't be empty!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateRegistrationNumber(String registrationNumber) {
+        if (registrationNumber.length() > 10) {
+            System.out.println("Registration number can't be longer than 10 chars!");
+            return false;
+        }
+        if (registrationNumber.isBlank()) {
+            System.out.println("Registration number can't be empty!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateBrand(String brand) {
+        if (brand.length() > 20) {
+            System.out.println("Brand can't be longer than 20 chars!");
+            return false;
+        }
+        if (brand.isBlank()) {
+            System.out.println("Brand can't be empty!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateModel(String model) {
+        if (model.length() > 20) {
+            System.out.println("Model can't be longer than 20 chars!");
+            return false;
+        }
+        if (model.isBlank()) {
+            System.out.println("Model can't be empty!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateFuelConsumption(float FuelConsumption) {
+        if (fuelConsumption <= 0) {
+            System.out.println("The fuel consumption can't be lower than 1!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateTankCapacity(int tankCapacity) {
+        if (tankCapacity <= 0) {
+            System.out.println("The tank capacity can't be lower than 1!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateHorsePower(int horsePower){
+        if(horsePower < 0){
+            System.out.println("The horse power can't be negative number!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateDates(LocalDate allDates){
+        if(nowDate.isBefore(allDates)){
+            System.out.println("Date can't be in the future!");
+            return false;
+        }
+        if(allDates.isBefore(negativeYear)){
+            System.out.println("Date can't be negative!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateDateOfChangeTires(LocalDate dateOfChangeTires){
+        if(dateOfChangeTires.isBefore(nowDate)){
+            System.out.println("The date can't be in the past!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateChangeOilKilometers(int kilometersChangeOil){
+        if(kilometersChangeOil < 0){
+            System.out.println("The kilometers can't be negative number!");
+            return false;
+        }
+        return true;
+    }
+
+
+
 
 
 }
