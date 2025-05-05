01b8127f6a4e00691531e11a92849f30002184f6
Iliya Raev <insomniq4@abv.bg>
2025-05-05 21:48:44 +0300

    Create Update car window class

---
+++ src/UpdateCarWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class UpdateCarWindow extends JFrame {
+    UpdateCarWindow(){
+        setTitle("Update car window");
+        setSize(700, 950);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+
+}
