680e8b47f47687c71731db56d706ccc8721d583f
Iliya Raev <insomniq4@abv.bg>
2025-05-06 17:47:43 +0300

    Main class where start project and create home window

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static List<Car> cars = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        new MainWindow();
+    }
+
+
+}
